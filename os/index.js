34a061fbe0d5252e702e01cdce44c34c39ec70e3
ashutosh mishra <mishraashutosh.dev@gmail.com>
2024-07-08 10:49:43 +0530

    learn about os module

---
+++ os/index.js
@@ -0,0 +1,34 @@
+
+const os = require('os');
+//console.log(os);
+
+//console.log("check cup architecture : ",os.arch()); // ans :  x64
+
+//console.log("check free memory : ",os.freemem());  // ans :  9930321920
+
+//console.log("check total memory : ",os.totalmem());  // ans :  16456368128
+
+//console.log("check network interface: ",JSON.stringify(os.networkInterfaces()));  
+//  ans :   {"Loopback Pseudo-Interface 1":[{"address":"::1","netmask":"ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff","family":"IPv6","mac":"00:00:00:00:00:00","internal":true,"cidr":"::1/128","scopeid":0},{"address":"127.0.0.1","netmask":"255.0.0.0","family":"IPv4","mac":"00:00:00:00:00:00","internal":true,"cidr":"127.0.0.1/8"}]}
+            
+//console.log("check default temp dir : ",os.tmpdir());    // ans :   check default temp dir :  C:\Users\ASHUTO~1\AppData\Local\Temp
+
+//console.log("check cpu status : ",os.cpus());  
+
+//console.log("check available Parallelism : ",os.availableParallelism());  // ans :  12
+
+//console.log("check endianness : ",os.endianness());  // ans :  LE
+
+//console.log("check Hostname : ",os.hostname());  // ans :  LAPTOP-H7UCDMBL
+
+//console.log("check Operating System : ",os.type());  // ans :  Windows_NT
+
+//console.log("check Operating plateform : ",os.platform());  // ans :  win32
+
+//console.log("check Operating systemtem release : ",os.release());  // ans :  10.0.22631
+
+//console.log("check Operating systemtem version : ",os.version());  // ans :  Windows 11 Home Single Language
+
+//console.log("check sytem user information : ",os.userInfo());  
+
+//console.log("check Operating systemtem machine : ",os.machine());   // ans : x86_64
